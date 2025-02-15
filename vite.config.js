ae210295d6a9fc2c726b68d10f8a398fd8b70cf4
avneetvirdi <avneetvirdi26@gmail.com>
2025-02-15 03:00:26 -0500

    mobile fixe

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/chatbot-gemini-ai"
+  base: "/chatbot-gemini-ai/"
 })
